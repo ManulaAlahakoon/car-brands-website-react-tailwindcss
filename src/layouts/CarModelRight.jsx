e89fc7cf2a0032e7e1c36ca111aefbf83e7bcd55
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-02-02 14:04:05 +0530

    A car model layout which is car in right side

---
+++ src/layouts/CarModelRight.jsx
@@ -0,0 +1,16 @@
+const CarModelRight = (props) => {
+    return (
+        <div className="grid grid-rows-1 md:grid-cols-2">
+
+            <div className="grid">
+                <img className="justify-self-center md:justify-self-end size-68 object-scale-down rounded-full" src={props.img} alt="img" />
+            </div>
+            <div className="text-center md:p-10 self-center text-4xl max-md:row-span-full">
+                <h3>{props.title}</h3>
+            </div>
+
+        </div>
+    );
+}
+ 
+export default CarModelRight;
