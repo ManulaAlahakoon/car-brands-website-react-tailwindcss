e49d22733400bf52e4aa7354b26d8eb18e5a9376
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-02-05 23:29:33 +0530

    Create Wonder section and its grid structure

---
+++ src/components/Wonder.jsx
@@ -0,0 +1,19 @@
+import icar from "../assets/wonder/apple-car.jpg"
+import cybertruck from "../assets/wonder/cybertruck.jpg"
+import vega from "../assets/wonder/vega-back.jpg"
+
+
+const Wonder = () => {
+  return (  
+    <div className="h-screen">
+      <h1>Wonder</h1>
+      <div className="grid   justify-center  ">
+        <div className="max-w-[384px] md:max-w-screen-md md:col-span-2"><img className="" src={vega} alt="" /></div>
+        <div className="max-w-[384px]"><img className=" " src={icar} alt="" /></div>
+        <div className="max-w-[384px]"><img className=" " src={cybertruck} alt="" /></div>
+      </div>
+    </div>
+  );
+}
+ 
+export default Wonder;
