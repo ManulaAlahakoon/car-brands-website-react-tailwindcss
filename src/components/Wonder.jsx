2ac832865dd49bbf801cc7739edf45f05244a891
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-07 23:56:01 +0530

    Update Wonder.jsx
    Updating section heading

--- src/components/Wonder.jsx
+++ src/components/Wonder.jsx
@@ -6,8 +6,8 @@ import vega from "../assets/wonder/vega-back.jpg"
 const Wonder = () => {
   return (  
     <div className="h-screen">
-      <h1>Wonder</h1>
-      <div className="grid   justify-center  ">
+      <h1 className="text-center text-3xl mt-24 mb-8">Wonder</h1>
+      <div className="grid justify-center">
         <div className="max-w-[384px] md:max-w-screen-md md:col-span-2"><img className="" src={vega} alt="" /></div>
         <div className="max-w-[384px]"><img className=" " src={icar} alt="" /></div>
         <div className="max-w-[384px]"><img className=" " src={cybertruck} alt="" /></div>
