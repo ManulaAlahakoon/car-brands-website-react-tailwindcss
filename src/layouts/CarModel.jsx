18b1a22bd1e2466687aabc82153c0e73f4eb3598
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-08 00:05:49 +0530

    Update CarModel.jsx
    Updating padding

--- src/layouts/CarModel.jsx
+++ src/layouts/CarModel.jsx
@@ -1,7 +1,7 @@
 const CarModel = (props) => {
     return (
         <div className="grid grid-rows-1 md:grid-cols-2">
-            <div className="text-center md:p-10 self-center text-4xl">
+            <div className="text-center p-10 md:p-10 self-center text-4xl">
                 <h3>{props.title}</h3>
             </div>
             <div className="grid">
