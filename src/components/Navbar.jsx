37e0a61cea67efb2ba0800e4af53c51a5b1bba7e
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-07 23:58:49 +0530

    Changing the site name & updating navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,12 +19,12 @@ const Navbar = () => {
     return ( 
         <div className="fixed w-full z-10">
             <div>
-                <div className="flex flex-row justify-between p-5 lg:px-32 bg-gradient-to-r from-blue-100 via-blue-300 to-blue-500">
+                <div className="flex flex-row justify-between p-5 lg:px-32 bg-gradient-to-r from-rose-100 to-teal-100">
                     <div className="flex flex-row items-center cursor-pointer gap-2">
                         <span >
                             <IoCarSportSharp className="size-10" />
                         </span>
-                        <h1 className="text-[24px]">Dream Cars</h1>
+                        <h1 className="text-[24px]">XtremeAuto</h1>
                     </div>
                     <nav className="hidden md:flex flex-row items-center gap-16">
                         <Link to="Brands" spy={true} smooth={true} duration={500} className="cursor-pointer first-letter:text-[20px]">Brands</Link>
@@ -45,7 +45,7 @@ const Navbar = () => {
                         )}
                     </div>
                 </div>
-                <div className={`${menu ? "translate-x-0":"-translate-x-full"} md:hidden flex flex-col gap-8 bg-gradient-to-r from-cyan-500 to-blue-500  text-white text-center text-[24px] absolute top-20 py-16 w-full h-fit transition duration-500 `}>
+                <div className={`${menu ? "translate-x-0":"-translate-x-full"} md:hidden flex flex-col gap-8 bg-gradient-to-r from-rose-100 to-teal-100  text-black text-center text-[24px] absolute top-20 py-16 w-full h-screen transition duration-500 `}>
                     <Link to="Brands" spy={true} smooth={true} duration={500} className="cursor-pointer first-letter:text-[24px]" onClick={closeMenu}>Brands</Link>
                     <Link to="Models" spy={true} smooth={true} duration={500} className="cursor-pointer first-letter:text-[24px]" onClick={closeMenu}>Models</Link>
                     <Link to="Wonder" spy={true} smooth={true} duration={500} className="cursor-pointer first-letter:text-[24px]" onClick={closeMenu}>Wonder</Link>
