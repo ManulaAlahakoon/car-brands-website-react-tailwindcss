cca8c08d4f2dd3a6f96256202c517957f596029b
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-08 00:05:57 +0530

    Update CarModelRight.jsx
    Updating padding

--- src/layouts/CarModelRight.jsx
+++ src/layouts/CarModelRight.jsx
@@ -5,7 +5,7 @@ const CarModelRight = (props) => {
             <div className="grid">
                 <img className="justify-self-center md:justify-self-end size-68 object-scale-down rounded-full" src={props.img} alt="img" />
             </div>
-            <div className="text-center md:p-10 self-center text-4xl max-md:row-span-full">
+            <div className="text-center p-10 md:p-10 self-center text-4xl max-md:row-span-full">
                 <h3>{props.title}</h3>
             </div>
 
