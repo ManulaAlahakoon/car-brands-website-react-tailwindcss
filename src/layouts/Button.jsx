c39b3e17e101674dbf7720dae5b07c1b3fd914c9
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-03-07 23:56:28 +0530

    Update Button.jsx
    Changing colors

--- src/layouts/Button.jsx
+++ src/layouts/Button.jsx
@@ -1,7 +1,7 @@
 const Button = (props) => {
     return (
         <div>
-            <button className="px-6 py-1 text-[18px] text-white font-medium rounded-full border-2 border-black  bg-gradient-to-r from-green-400 to-blue-500 hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 hover:text-white"  >
+            <button className="px-6 py-1 text-[18px] text-white font-medium rounded-full border-2 border-black  bg-black  hover:bg-white hover:text-black"  >
                 {props.title}
             </button>
         </div>
